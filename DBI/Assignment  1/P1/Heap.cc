2980bee49c1efa70dea285410e27e5c4a82627f4
Sasha Sequeira <gaurav.koolz@gmail.com>
2018-02-09 14:25:56 -0500

    Heap.cc Changes

--- DBI/Assignment  1/P1/Heap.cc
+++ DBI/Assignment  1/P1/Heap.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "TwoWayList.h"
 #include "Record.h"
 #include "Schema.h"
@@ -8,64 +9,111 @@
 #include "Heap.h"
 #include "Defs.h"
 
-Heap::Heap () {
+Heap::Heap()
+{
     //SET PAGE TO 0
     //CREATE NEW WBUFFER, RBUFFER AND REFFILE OBJECTS
-    currPageInd =0;
+    currPageInd = 0;
     wBuffer = new Page();
     rBuffer = new Page();
     refFile = new File();
-    dirtyPage =false;
+    dirtyPage = false;
 }
 
-Heap::~Heap () {
+Heap::~Heap()
+{
     //DESTROY THE OBJECT CREATE ABOVE
     delete wBuffer;
-    delete rBuffer ;
+    delete rBuffer;
     delete refFile;
 }
 
-int Heap::Create (const char *f_path, fType f_type, void *startup) {
+int Heap::Create(const char *f_path, fType f_type, void *startup)
+{
     //JUST RUN THE HEAP->OPEN WITH ZERO FLAG ON REFFILE
     refFile->Open(0, (char *)f_path);
     return 1;
 }
-//TODO: 
-void Heap::Load (Schema &f_schema, const char *loadpath) {
+
+void Heap::Load(Schema &f_schema, const char *loadpath)
+{
     //
+    FILE *table = fopen((char *)loadpath, "r");
+    //Check if file is empty
+    if (table == NULL)
+    {
+        //throw error
+        cerr << "File has no records";
+        exit(1);
+    }
+    else
+    {
+        //else read the record and add to page
+        Record temp;
+        while (temp.SuckNextRecord(&f_schema, table) == 1)
+        {
+            //Add will automatically assign which page the record gets addedto
+            Add(temp);
+        }
+
+        //Add would always leave the last page records in memory
+        //Write those records to another page.
+        //Dumping the memory contents
+        off_t currLen = refFile->GetLength();
+        off_t findPage = (currLen == 0) ? 0 : currLen - 1;
+        refFile->AddPage(wBuffer, findPage);
+    }
+    //FIXME: DEBUG
 }
 
-int Heap::Open (const char *f_path) {
-     refFile->Open(1, (char *)f_path);
+int Heap::Open(const char *f_path)
+{
+    refFile->Open(1, (char *)f_path);
     return 1;
 }
 
-void Heap::MoveFirst () {
+void Heap::MoveFirst()
+{
     currPageInd = 0;
 }
 
-int Heap::Close () {
-    return (refFile->Close()<0)?0:1;
+int Heap::Close()
+{
+    return (refFile->Close() < 0) ? 0 : 1;
 }
 
-
-void Heap::Add (Record &rec) {
+void Heap::Add(Record &rec)
+{
     //Check if buffer can be written to existing page
-    if(wBuffer->Append(&rec) == 0){
+    if (wBuffer->Append(&rec) == 0)
+    {
         //If not, dump the existing page
-        off_t currLen=refFile->GetLength();
-        off_t findPage = currLen ==0?0:currLen-1;
-        refFile->AddPage(wBuffer,findPage);
+        off_t currLen = refFile->GetLength();
+        off_t findPage = currLen == 0 ? 0 : currLen - 1;
+        refFile->AddPage(wBuffer, findPage);
         //Now delete the wBuffer
         wBuffer->EmptyItOut();
         //Append current records to the wBuffer
         wBuffer->Append(&rec);
-
     }
 }
-//TODO: 
-int Heap::GetNext (Record &fetchme) {
+//TODO:
+int Heap::GetNext(Record &fetchme)
+{
 }
-//TODO: 
-int Heap::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
+//TODO:
+int Heap::GetNext(Record &fetchme, CNF &cnf, Record &literal)
+{
+    bool compFlag = false;
+    ComparisonEngine engine;
+    while (!compFlag)
+    {
+        if (GetNext(fetchme) == 1)
+        {
+            compFlag = (bool)engine.Compare(&fetchme, &literal, &cnf);
+        }
+        else
+            return 0; //FIXME: try return false;
+    }
+    return (int)compFlag; //FIXME: try return compFlag;
 }
