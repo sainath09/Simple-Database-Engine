9b87e87e99f5e3c8d4935c8306a2542fb04b4204
Sasha Sequeira <gaurav.koolz@gmail.com>
2018-02-24 11:49:59 -0500

    Bug fixes for A2

--- DBI/Code/P1/BigQ.cc
+++ DBI/Code/P1/BigQ.cc
@@ -18,18 +18,25 @@
 //     return str;
 // }
 
-class sortOrder{
-	OrderMaker * om;
-public:
-	sortOrder(OrderMaker* omt){
-		om = omt;
-	}
-	bool operator()(Record* r1,Record* r2){
+// class sortOrder{
+// 	OrderMaker * om;
+// public:
+// 	sortOrder(OrderMaker* omt){
+// 		om = omt;
+// 	}
+// 	bool operator()(Record* r1,Record* r2){
+// 	ComparisonEngine comp;
+// 	if(comp.Compare(r1,r2,om)<0) return true;
+// 	else return false;
+// }
+// };
+OrderMaker *orderMaker;
+
+static bool sortOrder(Record* r1,Record* r2){
 	ComparisonEngine comp;
-	if(comp.Compare(r1,r2,om)<0) return true;
+	if(comp.Compare(r1,r2,orderMaker)<0) return true;
 	else return false;
 }
-};
 
 class sortMergeOrder{
 	OrderMaker* om;
@@ -54,7 +61,7 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 		return;
 	}
 	//initalizing global orderMaker
-	//orderMaker = &sortorder;
+	orderMaker = &sortorder;
 	//map for the overflow values
 	map<int,Page *> overflow;
 
@@ -72,7 +79,7 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 	// string metaFile(random_string(12)); 
 	// metaFile+="asdadasdads.dat";
 	char temp_file[100];
-	sprintf(temp_file,"temp%d.bin",rand());
+	sprintf(temp_file,"%d.bin",rand());
 	oFile.Open(0,temp_file); //Tempfile for maintaining sorted run
 	while(1){
 		// read data from in pipe sort them into runlen pages
@@ -90,78 +97,78 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 				totalPagesinRun++;
 			}
 		}
-			else { 
-				//In case all reads are over and they didnt fill a single page, 
-				//still add that incomplete page to vector
-				if(totalPagesinRun<runlen && tempPage->getNumRecs()) {
-					pVector.push_back(tempPage);
-				}
-				//TODO: Figure out if we are missing any source of information to push into vector
-				Record * readPVec;//Create a temp Record to dump values from pVector
+		else { 
+			//In case all reads are over and they didnt fill a single page, 
+			//still add that incomplete page to vector
+			if(totalPagesinRun<runlen && tempPage->getNumRecs()) {
+				pVector.push_back(tempPage);
+			}
+			//TODO: Figure out if we are missing any source of information to push into vector
+			Record * readPVec;//Create a temp Record to dump values from pVector
 
-				//Now read all the pages in pVector and sort and put them in rVector
-				for(int i=0;i<pVector.size();i++) {
+			//Now read all the pages in pVector and sort and put them in rVector
+			for(int i=0;i<pVector.size();i++) {
+				readPVec = new Record();
+				while(pVector[i]->GetFirst(readPVec)) {
+					rVector.push_back(readPVec);
 					readPVec = new Record();
-					while(pVector[i]->GetFirst(readPVec)) {
-						rVector.push_back(readPVec);
-						readPVec = new Record();
-					}
-					// Just dumping out values in correct garbage collection manner
-					delVar(readPVec);
-					delVar(pVector[i]);  
 				}
-				//Using standard sorting algo to sort all the records in the run
-				std::sort(rVector.begin(),rVector.end(),sortOrder(&sortorder));
+				//Just dumping out values in correct garbage collection manner
+				delVar(readPVec);
+				delVar(pVector[i]);  
+			}
+			//Using standard sorting algo to sort all the records in the run
+			std::sort(rVector.begin(),rVector.end(),sortOrder);
 
-				//Write the sorted record into pages
-				int pagesWritten=0; //TODO: page_count1
-				Page* wPage =new Page(); //TODO: file_page // temp page to write records to file 
-				for(int i=0;i<rVector.size();i++) {
-					//pageNotFull is also defined above in a different scope, just reusing that variable name here
-					bool pageNotFull = wPage->Append(rVector[i]);
-					if(!pageNotFull) {
+			//Write the sorted record into pages
+			int pagesWritten=0; //TODO: page_count1
+			Page* wPage =new Page(); //TODO: file_page // temp page to write records to file 
+			for(int i=0;i<rVector.size();i++) {
+				//pageNotFull is also defined above in a different scope, just reusing that variable name here
+				bool pageNotFull = wPage->Append(rVector[i]);
+				if(!pageNotFull) {
+					pagesWritten++;
+					oFile.AddPage(wPage,pageNumber++);
+					wPage->EmptyItOut();
+					wPage->Append(rVector[i]);
+				}
+				// Just dumping out values in correct garbage collection manner
+				delVar(rVector[i]);  
+			}
+			//now if there are some records in the end, write all of them in the last page of file
+
+			if(pagesWritten<runlen) {
+				//Include records which lie inside run length of a partial page read
+				//Only run if there are any actual records to read
+				if(wPage->getNumRecs()){
 						pagesWritten++;
 						oFile.AddPage(wPage,pageNumber++);
-						wPage->EmptyItOut();
-						wPage->Append(rVector[i]);
-					}
-					// Just dumping out values in correct garbage collection manner
-					delVar(rVector[i]);  
 				}
-				//now if there are some records in the end, write all of them in the last page of file
-
-				if(pagesWritten<runlen) {
-					//Include records which lie inside run length of a partial page read
-					//Only run if there are any actual records to read
-					if(wPage->getNumRecs()){
-							pagesWritten++;
-							oFile.AddPage(wPage,pageNumber++);
-					}
-				}
-				else { 
-					//Exlude records which lie outside run length, but have read partials pages into the run and send them to an overflow
-					//Only run if there are any actual records to read
-					if(wPage->getNumRecs()) {	
-						overflow[pageNumber-1]=wPage;
-					}
-				}
-				wPage=NULL;  
-				rVector.clear();
-				pVector.clear();
-
-				//Checking the status of pages to figure out if next run is going to start 
-				if (totalPagesinRun>=runlen) {
-					//Already have read the first record for next run if possible, append it here in available
-					tempPage->Append(&readRecord);
-					//Reset the number of pages in run, so that we can sort then next runlen records
-					totalPagesinRun=0;
-					continue;
-				}
-				else {
-					break;
-				}
-
 			}
+			else { 
+				//Exlude records which lie outside run length, but have read partials pages into the run and send them to an overflow
+				//Only run if there are any actual records to read
+				if(wPage->getNumRecs()) {	
+					overflow[pageNumber-1]=wPage;
+				}
+			}
+			wPage=NULL;  
+			rVector.clear();
+			pVector.clear();
+
+			//Checking the status of pages to figure out if next run is going to start 
+			if (totalPagesinRun>=runlen) {
+				//Already have read the first record for next run if possible, append it here in available
+				tempPage->Append(&readRecord);
+				//Reset the number of pages in run, so that we can sort then next runlen records
+				totalPagesinRun=0;
+				continue;
+			}
+			else {
+				break;
+			}
+
+		}
 		
 
 	} //End of while
@@ -230,7 +237,7 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 				
 				oFile.GetPage(tempPageInRun[recBelongsinRun],pagesInRun[recBelongsinRun]+recBelongsinRun*runlen);
 				tempPageInRun[recBelongsinRun]->GetFirst(findNextRecordinRun);
-				tempPageInRun[recBelongsinRun]++;
+				pagesInRun[recBelongsinRun]++;
 			}
 			else {  
 				if(pagesInRun[recBelongsinRun]==runlen)
