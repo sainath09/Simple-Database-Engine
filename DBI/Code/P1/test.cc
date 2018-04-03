2445d53cd988000ef4a3176da09cde75f8d5bed9
KPS <sainath@ufl.edu>
2018-04-03 19:48:31 -0400

    test.cc commit

--- DBI/Code/P1/test.cc
+++ DBI/Code/P1/test.cc
@@ -35,7 +35,7 @@ int clear_pipe (Pipe &in_pipe, Schema *schema, Function &func, bool print) {
 	cout << " Sum: " << sum << endl;
 	return cnt;
 }
-int pipesz = 100; // buffer sz allowed for each pipe
+int pipesz = 10000000; // buffer sz allowed for each pipe
 int buffsz = 100; // pages of memory allowed for operations
 
 SelectFile SF_ps, SF_p, SF_s, SF_o, SF_li, SF_c;
@@ -204,7 +204,7 @@ void q4 () {
 	
 	SF_ps.Run (dbf_ps, _ps, cnf_ps, lit_ps); // 161 recs qualified
 	
-	J.Run (_s, _ps, _s_ps, cnf_p_ps, lit_p_ps,s->schema(),ps->schema());
+	J.Run (_s, _ps, _s_ps, cnf_p_ps, lit_p_ps);
 	
 	T.Run (_s_ps, _out, func);
 	
@@ -297,30 +297,29 @@ void q6 () {
 	G.Use_n_Pages (1);
 
 	SF_ps.Run (dbf_ps, _ps, cnf_ps, lit_ps); // 161 recs qualified
-	J.Run (_s, _ps, _s_ps, cnf_p_ps, lit_p_ps,s->schema(),ps->schema());
+	J.Run (_s, _ps, _s_ps, cnf_p_ps, lit_p_ps);
 	G.Run (_s_ps, _out, grp_order, func);
 
-	G.WaitUntilDone ();
-	J.WaitUntilDone ();
 	SF_ps.WaitUntilDone ();
+	J.WaitUntilDone ();
 
 	Schema sum_sch ("sum_sch", 1, &DA);
 	int cnt = clear_pipe (_out, &sum_sch, true);
 	cout << " query6 returned sum for " << cnt << " groups (expected 25 groups)\n"; 
+	G.WaitUntilDone ();
+
+	
 }
 
 void q7 () {
 
 	cout << " query7 for testing group by with out join \n";
-	
-	
-
 	char *pred_ps = "(ps_suppkey = ps_suppkey)";
 	init_SF_ps (pred_ps, 100);
 
 	GroupBy G;
 		// _s (input pipe)
-	Pipe _out (10000);
+	Pipe _out (1);
 	Function func;
 	char *str_sum = "(ps_supplycost)";
 	get_cnf (str_sum, ps->schema(), func);
@@ -331,11 +330,11 @@ void q7 () {
 	SF_ps.Run (dbf_ps, _ps, cnf_ps, lit_ps); // 161 recs qualified
 	G.Run (_ps, _out, grp_order, func);
 
+	G.WaitUntilDone ();
 	Schema sum_sch ("sum_sch", 1, &DA);
 	SF_ps.WaitUntilDone ();
 	int cnt = clear_pipe (_out, &sum_sch, true);
 	cout << " query7 returned sum for " << cnt << " groups (expected 25 groups)\n"; 
-	G.WaitUntilDone ();
 
 }
 
