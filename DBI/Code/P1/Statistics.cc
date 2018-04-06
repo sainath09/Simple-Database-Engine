c6f7186fd7ef2b2cd31cd36ae11dc6ee563025dc
KPS <sainath@ufl.edu>
2018-04-06 16:47:05 -0400

    completed estimate Found bugs in apply working to remove them

--- DBI/Code/P1/Statistics.cc
+++ DBI/Code/P1/Statistics.cc
@@ -36,12 +36,21 @@ void Statistics::CopyRel(char *oldName, char *newName)
     toCopy.numRel = mapRel[oldname].numRel;
     for(auto it_attr = mapRel[oldname].mapAttr.begin();it_attr != mapRel[oldname].mapAttr.end();it_attr++ ){
 
-        toCopy.mapAttr.insert(make_pair(it_attr->first,it_attr->second));
+        toCopy.mapAttr[it_attr->first] = it_attr->second;
     }
     mapRel[newname] = toCopy;
+
+    // cout<<"BEGIN PRINT"<<endl;
+    // for(auto it = mapRel[newname].mapAttr.begin();it!= mapRel[newname].mapAttr.end();it++){
+    //     cout<<it->first<<endl;
+    //     cout<<it->second<<endl;
+    // }
+    // cout<<"end PRINT"<<endl;
 }
 	
 void Statistics::Read(char *fromWhere){
+    mapRel.clear();
+    resFromEstimate = 0.0;
     ifstream in(fromWhere);
     if(!in.is_open()){
         cerr<<"No file to read Please check the file path or that file exists!";
@@ -49,7 +58,9 @@ void Statistics::Read(char *fromWhere){
     }
     string line;
     getline(in,line); //Dumping out startOfRelation here
+    
     while(getline(in,line)){
+       // cout<<"Relation name"<<line<<endl;
         if(line == "RelationName:"){
             string relname;
             in>>relname;
@@ -58,17 +69,29 @@ void Statistics::Read(char *fromWhere){
             in>>tempRel.numRel;
             in>>line; // Dumping Attrs to line
             in>>tempRel.numTuples;
+            getline(in,line);
+            
             while(getline(in,line) && line != "startOfRelation" && line !="EOF"){
+               // cout<<"line:"<<line<<endl;
                 string attrName;
                  attrName = line;
                 in>>tempRel.mapAttr[attrName];
+                getline(in,line);
             }
             mapRel[relname] = tempRel;
 
         }
-
-
     }
+    // cout<<"BEGIN PRINT"<<endl;
+    // for(auto it = mapRel.begin();it!= mapRel.end();it++){
+    //     cout<<it->first<<endl;
+    //     cout<<it->second.numTuples<<endl;
+    //     cout<<it->second.numRel<<endl;
+    //     for(auto itt = it->second.mapAttr.begin();itt!= it->second.mapAttr.end();itt++ ){
+    //         cout<<itt->first<<":"<<itt->second<<endl;
+    //     }
+    // }
+    // cout<<"end PRINT"<<endl;
 
 }
 void Statistics::Write(char *fromWhere){
@@ -113,7 +136,7 @@ void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJo
                             break;
                         }
                     }
-                    string RelationName = relIt[0]->first + "|" + relIt[1]->second;
+                    string RelationName = relIt[0]->first +"|"+ relIt[1]->first;
                     tempjoinRelation.numTuples = resFromEstimate;
                     tempjoinRelation.numRel = numToJoin;
                     for(int i = 0;i<2;i++){
@@ -142,7 +165,74 @@ void  Statistics::Apply(struct AndList *parseTree, char *relNames[], int numToJo
          tempParser = tempParser->rightAnd;
     }
 }
-double Statistics::Estimate(struct AndList *parseTree, char **relNames, int numToJoin)
-{
+double Statistics::Estimate(struct AndList *parseTree, char **relNames, int numToJoin){
+    struct AndList* tempParser = parseTree;
+    struct OrList* innerOrList;
+    double res = 0.0,frac = 1.0;
+    int FLAG = 0;
+    if(!tempParser){
+        if(numToJoin>1) return -1;
+        return mapRel[relNames[0]].numTuples;
+    }
+    while(tempParser){
+        if(tempParser->left){
+            innerOrList = tempParser->left;
+            double orFrac = 0.0;
+            unordered_map<string,int>::iterator attItEnd;
+            while(innerOrList){
+                if(innerOrList->left->left->code == 4 && innerOrList->left->right->code == 4){ //FIXME: 4 is for string and 3 is for name
+                    unordered_map<string,int>::iterator attIt[2];
+                    unordered_map<string,structRel>::iterator relIt[2];
+                    structRel tempjoinRelation;
+                    for(auto it = mapRel.begin();it!= mapRel.end();it++ ){
+                        attIt[0] = it->second.mapAttr.find(innerOrList->left->left->value);
+                        if(attIt[0] != it->second.mapAttr.end()){
+                            relIt[0] = it;
+                            break;
+                        }
+                    }
+                    for(auto it = mapRel.begin();it!= mapRel.end();it++ ){
+                        attIt[1] = it->second.mapAttr.find(innerOrList->left->right->value);
+                        if(attIt[1] != it->second.mapAttr.end()){
+                            relIt[1] = it;
+                            break;
+                        }
+                    }
+                    double maxi = (double)max(attIt[0]->second,attIt[1]->second);
+                    if(FLAG == 0)
+                        res =  (double)relIt[0]->second.numTuples*(double)relIt[1]->second.numTuples/maxi;
+                    else
+                        res *= (double)relIt[1]->second.numTuples/maxi;
+                    FLAG = 1;
+                }
+                else{
+                    unordered_map<string,int>::iterator attIt;
+                    unordered_map<string,structRel>::iterator relIt;
+                    for(auto it = mapRel.begin();it!=mapRel.end();it++){
+                        attIt = it->second.mapAttr.find(innerOrList->left->left->value);
+                        if(attIt != it->second.mapAttr.end()){
+                            relIt = it;
+                            break;
+                        }
+                    }
+                    if(res == 0.0) res = ((double)relIt->second.numTuples);
+                    double tempf;
+                    if(innerOrList->left->code == 3) tempf = 1.0/attIt->second; //FIXME: value of equals
+                    else tempf = 1.0/3.0;
+                    if(attIt != attItEnd)
+                        orFrac = tempf + orFrac - (tempf*orFrac);
+                    else  
+                        orFrac += tempf;
+                    attItEnd = attIt;
+                }
+                innerOrList = innerOrList->rightOr;
+            }
+            if(orFrac != 0.0) frac *= orFrac;
+        }
+        tempParser = tempParser->rightAnd;       
+    }
+    res *= frac;
+    resFromEstimate = res;
+    return res;
 }
 
